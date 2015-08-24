13fcb29209f71164e0248cc9338bc95f86108e0f
yyong <coolfish2004@hotmail.com>
2015-08-24 19:30:08 +0800

    从历史版本重新修改的代码，1.修复断点续传报sign校验错误的问题，2.完成获取播放视频接口功能

---
+++ config.js
@@ -0,0 +1,12 @@
+var config = {
+	// 服务器所在ip
+	serverUrl: "116.226.35.96",
+	// 用户标识符
+	user_unique : "fcba45089f",
+	// 用户密钥
+	secret_key : "768cabd0d7806dcd4da13586029be607",
+	// 乐视云api提交地址
+	// letvServer:"http://api.letvcloud.com/open.php"
+}
+
+module.exports = config;
